8687e63fa8861093ee4a1bcd7cf8eaaf545cca1b
android_wxr@163.com <549661646@qq.com>
2018-05-15 22:54:12 +0800

    提交dongnao的view

---
+++ app/src/main/java/com/wxj/customview/dongnao/PaintActivity.java
@@ -0,0 +1,21 @@
+package com.wxj.customview.dongnao;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.wxj.customview.R;
+
+/**
+ * Created by wuxiaojun on 2018/1/8.
+ */
+
+public class PaintActivity extends Activity {
+
+	@Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_paint);
+
+	}
+
+}
